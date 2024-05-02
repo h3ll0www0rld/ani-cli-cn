382042473bb45a42c8deb1c379f478d7558d744d
h3ll0www0rld <zhuoyicheng2008@163.com>
2024-05-02 16:28:01 +0800

    fix: 修复build.yaml

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,27 +1,22 @@
 $env:PYPPETEER_CHROMIUM_REVISION='1263111'
 pip install -r requirements.txt
-$filePath = "c:\hostedtoolcache\windows\python\3.11.9\x64\lib\site-packages\pyppeteer\chromium_downloader.py"
 # 修改chromium镜像位置
-$lineNumber = 29
-$newLineContent = 'DOWNLOAD_HOST = "https://registry.npmmirror.com/-/binary"'
-
-(Get-Content $filePath) | ForEach-Object {
-    if ($_.ReadCount -eq $lineNumber) {
-        $_ -replace 'DOWNLOAD_HOST = ".+"', $newLineContent
-    } else {
-        $_
+$filePath = "c:\hostedtoolcache\windows\python\3.11.9\x64\lib\site-packages\pyppeteer\chromium_downloader.py"
+$content = Get-Content $filePath
+$content | ForEach-Object {
+    if ($_ -match "REVISION") {
+        $_ = 'REVISION = "1294824"'
     }
-} | Set-Content $filePath
+    $_
+} | Set-Content $file -Force
 
-$lineNumber = 32
-$newLineContent = 'REVISION = "1294824"'
-
-(Get-Content $filePath) | ForEach-Object {
-    if ($_.ReadCount -eq $lineNumber) {
-        $_ -replace 'DOWNLOAD_HOST = ".+"', $newLineContent
-    } else {
-        $_
+$content = Get-Content $filePath
+$content | ForEach-Object {
+    if ($_ -match "DOWNLOAD_HOST") {
+        $_ = 'DOWNLOAD_HOST = "https://registry.npmmirror.com/-/binary"'
     }
-} | Set-Content $filePath
+    $_
+} | Set-Content $file -Force
+
 pyinstaller -F main.py
 Rename-Item -Path "./dist/main.exe" -NewName "ani-cli-cn.exe"
