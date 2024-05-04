f281fb9eac27d68041f8e1a4e8170dfdc5b76e61
h3ll0www0rld <zhuoyicheng2008@163.com>
2024-05-04 11:21:44 +0800

    fix: 修复linux下chromium下载位置问题

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ import chromium_downloader
 
 if __name__ == "__main__":
     username = os.environ.get("USERNAME")
-    path = (
-        f"C:\\Users\\{username}\\AppData\\Local\\pyppeteer\\pyppeteer\\local-chromium"
-    )
-    if os.path.exists(path) != True:
+    if os.path.exists(chromium_downloader.DOWNLOADS_FOLDER) != True:
         print("检测到未下载chromium，开始下载...")
         chromium_downloader.download_chromium()
         os.rename(
-            f"C:\\Users\\{username}\\AppData\\Local\\pyppeteer\\pyppeteer\\local-chromium\\1294824",
-            f"C:\\Users\\{username}\\AppData\\Local\\pyppeteer\\pyppeteer\\local-chromium\\1181205",
+            os.path.join(chromium_downloader.DOWNLOADS_FOLDER,"1294824"),
+            os.path.join(chromium_downloader.DOWNLOADS_FOLDER,"1181205"),
         )
         print("下载完成")
     Tui.main()
